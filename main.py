46f0da064ec86cd0d27c8492a13cc790efbcbb4f
IvanS <x-sakaev@yandex.ru>
2024-07-18 13:21:41 +0300

    camera is binded to the head

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ from sprites.snake import Snake
 
 
 # Константы
-WIDTH = 500
-HEIGHT = 500
+WIDTH = 750
+HEIGHT = 750
+GAME_WIDTH = 1000
+GAME_HEIGHT = 1000
 FPS = 60
 SNAKE_SIZE = 10
 
@@ -22,7 +24,8 @@ clock = pygame.time.Clock()
 
 
 # Спрайты
-snake = Snake(SNAKE_SIZE)
+snake = Snake(SNAKE_SIZE, screen_w=WIDTH, screen_h=HEIGHT,
+              game_w=GAME_WIDTH, game_h=GAME_HEIGHT)
 
 
 
@@ -45,14 +48,26 @@ while running:
             mouse_y = event.pos[1]
 
 
+    # Обновление спрайтов
+    snake.update(mouse_x, mouse_y)
+    head_x, head_y = snake.get_head_position()
+
+
     # Рендеринг
     screen.fill((255, 255, 255))
     snake.draw(screen)
-
-
-    # Обновление спрайтов
-    snake.update(mouse_x, mouse_y)
-
+    pygame.draw.line(screen, (0, 0, 0),
+                     (WIDTH / 2 - head_x, HEIGHT / 2 - head_y),
+                     (WIDTH / 2 - head_x + GAME_WIDTH, HEIGHT / 2 - head_y), 3)
+    pygame.draw.line(screen, (0, 0, 0),
+                     (WIDTH / 2 - head_x, HEIGHT / 2 - head_y + GAME_HEIGHT),
+                     (WIDTH / 2 - head_x + GAME_WIDTH, HEIGHT / 2 - head_y + GAME_HEIGHT), 3)
+    pygame.draw.line(screen, (0, 0, 0),
+                     (WIDTH / 2 - head_x, HEIGHT / 2 - head_y),
+                     (WIDTH / 2 - head_x, HEIGHT / 2 - head_y + GAME_HEIGHT), 3)
+    pygame.draw.line(screen, (0, 0, 0),
+                     (WIDTH / 2 - head_x + GAME_WIDTH, HEIGHT / 2 - head_y),
+                     (WIDTH / 2 - head_x + GAME_WIDTH, HEIGHT / 2 - head_y + GAME_HEIGHT), 3)
 
     # Обновление экрана
     pygame.display.update()
--- sprites/snake.py
+++ sprites/snake.py
@@ -3,36 +3,47 @@ import vector
 
 
 class Snake:
-    def __init__(self, length, fragment_size=10, dot_size=2, image_path="assets/snake_fragment.png"):
-        self.snake_list = [vector.obj(x = (i*dot_size) + 10, y = 10)
-                           for i in range(length * fragment_size + 1)]
+    def __init__(self, length, fragment_size=10, dot_size=2, screen_w=500,
+                 screen_h=500, game_w=1000, game_h=1000,
+                 image_path="assets/snake_fragment.png"):
+        
+        self.snake_list = [vector.obj(x = 10, y = 10)]
         self.length = length
         self.fragment_size = fragment_size
         self.dot_size = dot_size
+        self.add_dots = (length - 1) * fragment_size
+
+        self.screen_w = screen_w
+        self.screen_h = screen_h
+        self.game_w = game_w
+        self.game_h = game_h
 
         self.image = pygame.image.load(image_path)
         
-    def move(self, angle, need_move_head):
-        if need_move_head:
-            v = vector.obj(rho=self.dot_size, phi=angle)
-            self.snake_list.append(self.snake_list[-1] + v)
+    def move(self, angle):
+        v = vector.obj(rho=self.dot_size, phi=angle)
+        self.snake_list.append(self.snake_list[-1] + v)
+        if self.add_dots <= 0:
+            self.snake_list.pop(0)
         else:
-            self.snake_list.append(self.snake_list[-1])
-        self.snake_list.pop(0)
+            self.add_dots -= 1
     
     def draw(self, surface):
-        for i in range(0, len(self.snake_list), self.fragment_size):
+        head_pos = self.get_head_position()
+        head_v = vector.obj(x=head_pos[0], y=head_pos[1])
+        for i in range((len(self.snake_list)-1) % self.fragment_size,
+                       len(self.snake_list), self.fragment_size):
+            draw_v = self.snake_list[i] - head_v
             rect = self.image.get_rect()
-            rect.centerx = self.snake_list[i].x
-            rect.centery = self.snake_list[i].y
+            rect.centerx = draw_v.x + self.screen_w / 2
+            rect.centery = draw_v.y + self.screen_h / 2
             surface.blit(self.image, rect)
 
     def update(self, mouse_x, mouse_y):
-        head_pos = self.get_head_position()
-        head_v = vector.obj(x=head_pos[0], y=head_pos[1])
-        mouse_v = vector.obj(x=mouse_x, y=mouse_y)
-        head_to_mouse_v = mouse_v - head_v
-        self.move(head_to_mouse_v.phi, head_to_mouse_v.rho >= self.dot_size)
+        x = mouse_x - self.screen_w / 2
+        y = mouse_y - self.screen_h / 2
+        mouse_v = vector.obj(x=x, y=y)
+        self.move(mouse_v.phi)
 
     def get_head_position(self):
         return (self.snake_list[-1].x, self.snake_list[-1].y)
